8f98fc4bd1a4f97344130200f53c12affc7fd245
mxrlxw <121444190+nsa-dev@users.noreply.github.com>
2023-10-26 17:45:50 +0200

    Revert "Comment & fix coquille"
    This reverts commit 559bec2dcb33f6f5d4e248e82b81f628ff9c2ad8.

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<!-- ERREUR : Include mal orthographié -->
-
-<!-- Appel au fichier qui regroupe les fonctions du programme pour accès ici-->
-<?php include 'inc/inc.functions.php'?>
-
-
+<?php includ 'inc/inc.functions.php'; ?>
 <!DOCTYPE HTML>
 <!--
 	Story by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<!-- Pas d'attribut lang -->
-<html lang="fr"> 
+<html>
 	<head>
 		<title>Story by HTML5 UP</title>
 		<meta charset="utf-8" />
@@ -19,22 +13,18 @@
 		<?php include 'inc/inc.css.php'; ?>
 	</head>
 	<body class="is-preload">
+
 		<!-- Wrapper -->
 			<div id="wrapper" class="divided">
 				<?php 
-				// ERREUR : Fonction disponible dans le fichier inc/inc.functions.php mal orthographié s en trop
-					getPageTemplate(
-						// ERREUR : fonction php mal othographié
-						// On passe soit le nom du template, soit null à la fonction
-						array_key_exists('page', $_GET) ? $_GET['page'] : null
+					getPagesTemplate(
+						array_key_exist('page', $_GET) ? $_GET['page'] : null
 					); 
 				?>
-				<!-- ERREUR : nom de fichier mal orthographié -->
-				<?php include 'inc/tpl-footer.php'; ?>
+				<?php include 'inc/tpls-footer.php'; ?>
 			</div>
 
-		<!-- ERREUR : Include mal orthographié -->
-		<?php include 'inc/inc.js.php'; ?>
+		<?php includes 'inc/inc.js.php'; ?>
 
 	</body>
 </html>
