12d52d4a9109fc1a5e2a1a3cd8271847e5a4081a
Damien Cuda <damien.cuda@gmail.com>
2023-10-26 17:01:47 +0200

    Optimisation de l'affichage du bouton se connecter

--- inc/tpl-footer.php
+++ inc/tpl-footer.php
@@ -5,22 +5,28 @@
 	</div>
 </footer>
 
-<!-- Sticky -->	
-<span class="sticky">
-	<?php 
-		if(!isConnected()){		// si déconnecté
-			?>
-				<a class="button fit" href="?page=login" title="Se connecter à l'administration du blog">
-	                <span class="logo icon fa-arrow-alt-circle-right">Se connecter</span>
-	            </a>
-            <?php
-		}else{					// si connecté
-			?>
-				<a class="button fit" href="?page=disconnect" title="Se déconnecter de l'administration du blog">
-	                <span class="logo icon fa-arrow-alt-circle-right">Se déconnecter</span>
-	            </a>
-	            
-			<?php
-		}	
-	?>
-</span>
+<!-- Sticky -->
+<!-- <?php
+// Le bouton se connecter n'apparait pas si l'on est sur la page de connection
+ if(!isset($_GET['page']) || ($_GET['page'] != 'login')){ ?> -->
+	<span class="sticky">
+		<?php 
+			if(!isConnected()){		// si déconnecté
+				?>
+					<a class="button fit" href="?page=login" title="Se connecter à l'administration du blog">
+						<span class="logo icon fa-arrow-alt-circle-right">Se connecter</span>
+					</a>
+				<?php
+			}else{					// si connecté
+				?>
+					<a class="button fit" href="?page=disconnect" title="Se déconnecter de l'administration du blog">
+						<span class="logo icon fa-arrow-alt-circle-right">Se déconnecter</span>
+					</a>
+					
+				<?php
+			}	
+		?>
+	</span>
+	<?php
+}
+?>
