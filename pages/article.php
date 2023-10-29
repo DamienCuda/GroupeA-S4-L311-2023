130919bac682f7023899493aa594fab974a7c08f
Damien Cuda <damien.cuda@gmail.com>
2023-10-29 12:27:22 +0100

    bug fix and comment

--- pages/article.php
+++ pages/article.php
@@ -3,11 +3,12 @@
 	$article = getArticleById(array_key_exists('id', $_GET) ? $_GET['id'] : null);
 
 	// Si on un retour null de la fonction getArticleById ou si !count renvoi 1 (Empty array)
-	// On est rediriger vers l'accueil 
+	// On est redirigé vers l'accueil 
 	if(is_null($article) or !count($article)){ // ERREUR : Point d'exclamation en TROP 😆 avant la fonction count
 		header('Location:index.php');
 	}
-?>	
+?>
+<!-- Sinon affichage article -->
 <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 	<div class="content">
 		<h1><?php echo $article['titre'];?></h1>
@@ -17,6 +18,6 @@
 		</ul>
 	</div>
 	<div class="image">
-		<img src="<?php echo $art['image'];?>" alt="" />
+		<img src="<?php echo $article['image'];?>" alt="" />  <!-- ERREUR : coquille variable $article -->
 	</div>
 </section>
